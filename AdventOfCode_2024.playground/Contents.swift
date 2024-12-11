9c96aff405e6baef3067cf3ce30d73b3498b2168
Fani Jasmakovska <fani.jasmakovska@gmail.com>
2024-12-11 03:51:16 +0100

    Added 2024 - day 6 part 2 solution.

--- AdventOfCode_2024.playground/Contents.swift
+++ AdventOfCode_2024.playground/Contents.swift
@@ -16,4 +16,5 @@ import Foundation
 //Day5.part1()
 //Day5.part2()
 
-Day6.part1()
+//Day6.part1()
+Day6.part2()
--- AdventOfCode_2024.playground/Sources/Day6.swift
+++ AdventOfCode_2024.playground/Sources/Day6.swift
@@ -24,7 +24,7 @@ public struct Day6 {
         var moved = true
         var change = false
         while moved{
-            (moved, change)  = guardLoc?.move() ?? (false, false)
+            (moved, change) = guardLoc?.move() ?? (false, false)
             if change {
                 guardLoc?.directionChange()
                 moved = true
@@ -34,6 +34,68 @@ public struct Day6 {
         print(guardLoc?.visited.count ?? 0)
     }
     
+    public static func part2() {
+        let text = Bundle.main.getInput(file: "input_day6")?.trimmingCharacters(in: .whitespaces)
+        let lines = text!.split(separator: "\n").map{ Array($0) }
+        
+        var guardLoc:Point?
+        var obstacles = Set<Point>()
+        
+        for (x, line) in lines.enumerated(){
+            for (y, char) in line.enumerated(){
+                if char == "^"{
+                    guardLoc = Point(x: x, y: y, direction: .up, maxX: lines.count, maxY: line.count)
+                }
+                else if char == "#"{
+                    obstacles.insert(Point(x: x, y: y))
+                }
+            }
+            guardLoc?.obstacles = obstacles
+        }
+        
+        var loops = 0
+        
+        if let guardLoc = guardLoc {
+            for (x, line) in lines.enumerated(){
+                for (y, _) in line.enumerated(){
+                    let point = Point(x: x, y: y)
+                    
+                    if !obstacles.contains(point) && !(point.x == guardLoc.x && point.y == guardLoc.y){
+                        if obstacles.map({ $0.x == x }).count > 0 || obstacles.map({ $0.y == y }).count > 0 {
+                            
+                            let guardC = guardLoc.copy() as! Day6.Point
+                            guardC.obstacles.insert(point)
+                            
+                            var moved = true
+                            var change = false
+                            while moved{
+                                (moved, change) = guardC.move()
+                                if change {
+                                    if let _ = point.visited.filter({
+                                        $0.x == guardC.x &&
+                                        $0.y == guardC.y &&
+                                        $0.direction == guardC.direction
+                                    }).first{
+                                        
+                                        moved = false
+                                        loops += 1
+                                        
+                                        continue
+                                    }
+                                    
+                                    point.visited.insert(Point(x: guardC.x, y: guardC.y, direction: guardC.direction))
+                                    guardC.directionChange()
+                                    moved = true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        print(loops)
+    }
 }
 
 extension Day6{
@@ -58,7 +120,7 @@ extension Day6{
         }
     }
     
-    class Point: NSObject{
+    class Point: NSObject, NSCopying{
         var x:Int
         var y:Int
         
@@ -125,9 +187,15 @@ extension Day6{
             else{
                 self.x = temp.x
                 self.y = temp.y
-                visited.insert(Point(x: self.x, y: self.y))
+                visited.insert(Point(x: self.x, y: self.y, direction: self.direction))
                 return (true, false)
             }
+        }
+        
+        func copy(with zone: NSZone? = nil) -> Any {
+            let copy = Point(x: x, y: y, direction: direction, maxX: maxX, maxY: maxY)
+            copy.obstacles = obstacles
+            return copy
         }
     }
 }
